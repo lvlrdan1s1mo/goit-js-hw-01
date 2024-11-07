735af7ec064cb32f9d9b1da90de0ede76ba132eb
lvlrdan1s1mo <danyamalaev1@gmail.com>
2024-11-07 17:39:20 +0100

    Add files via upload

--- js/task-3.js
+++ js/task-3.js
@@ -6,6 +6,6 @@ const borderWibth = parseFloat(border) * 2;
 const totalWibth = contentWibth + paddingWibth + borderWibth;
 return totalWibth;
 }
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(number.getElementWidth("50px", "8px", "4px"));
+console.log(number.getElementWidth("60px", "12px", "8.5px"));
+console.log(number.getElementWidth("200px", "0px", "0px"));
